dc276218cd9e966a28c56d66e233c63a1d78d735
Felhasználó <Felhasznl@DESKTOP-LHR835G>
2022-11-03 17:27:38 +0100

    otodik

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using WindowsFormsApp1.Entities;
 using WindowsFormsApp1.MnbServiceReference;
 
@@ -21,7 +22,10 @@ namespace WindowsFormsApp1
             InitializeComponent();
 
             dataGridView1.DataSource = Rates;
+        }
 
+        private string ServiceCall()
+        {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
@@ -33,8 +37,35 @@ namespace WindowsFormsApp1
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
         }
+
+        private void ProcessXML()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(ServiceCall());
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                if (childElement == null)
+                    continue;
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+
+            }
+        }
+
+
     }
 
-        
+
 }
     
